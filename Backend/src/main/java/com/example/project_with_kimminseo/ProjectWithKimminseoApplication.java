9e9d09737669a44387e76c5d13e4cb2208da6ec6
unname <lwer210@naver.com>
2024-10-01 14:35:10 +0900

    Base Backend Code

---
+++ Backend/src/main/java/com/example/project_with_kimminseo/ProjectWithKimminseoApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_with_kimminseo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWithKimminseoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWithKimminseoApplication.class, args);
+	}
+
+}
